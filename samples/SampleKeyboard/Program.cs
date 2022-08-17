2f30caa39c0998a63c9650cba2bea74e27fc0fc5
Nate Yocom <nate@yocom.org>
2022-08-17 16:48:25 -0700

    Add samples

---
+++ samples/SampleKeyboard/Program.cs
@@ -0,0 +1,8 @@
+﻿using Nfw.Linux.Hid.Keyboard;
+
+Console.WriteLine("Outputting \"test\" to keyboard on /dev/hidg0 in 5 seconds...");
+Thread.Sleep(5000);
+Console.WriteLine("... now");
+
+SimpleKeyboard keyboard = new SimpleKeyboard();
+keyboard.EmitString("test");
---
+++ samples/SampleMouse/Program.cs
@@ -0,0 +1,16 @@
+﻿using Nfw.Linux.Hid.Mouse;
+
+Console.WriteLine("Moving the mouse corner to corner in 5 seconds...");
+Thread.Sleep(5000);
+Console.WriteLine("... now");
+
+SimpleMouse mouse = new SimpleMouse();
+
+mouse.Center();
+
+for(int x = 10; x < SimpleMouse.MAX_X; x++) {    
+    mouse.MoveTo(x,x);
+    Thread.Sleep(1);    
+}
+
+Console.WriteLine("... Done");
